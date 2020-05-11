c4fa2d38488d53995729df5c3d22307536c9cf3c
Krishna Kuruvadi <krishnakuruvadi@gmail.com>
2020-05-11 17:06:58 +0530

    Initial commit with PPF

---
+++ src/pages/views.py
@@ -0,0 +1,10 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs): # *args, **kwargs
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
---
+++ src/ppf/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PpfConfig(AppConfig):
+    name = 'ppf'
