b976e6ff29435c3f39a6c05d4cde175b97d2318f
Krishna Kuruvadi <kkuruvad@cisco.com>
2020-05-19 13:16:41 +0530

    Add support for fixed deposit

---
+++ src/fixed_deposit/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView
+)
+from django.template import Context
+from decimal import Decimal
+from .models import FixedDeposit
+from .fixed_deposit_helper import add_fd_entry, get_maturity_value
+
+# Create your views here.
+
+class FixedDepositListView(ListView):
+    template_name = 'fixed-deposits/fixed_deposit_list.html'
+    queryset = FixedDeposit.objects.all() # <blog>/<modelname>_list.html
+
+class FixedDepositDetailView(DetailView):
+    template_name = 'fixed-deposits/fixed_deposit_detail.html'
+    #queryset = Ppf.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(FixedDeposit, id=id_)
+
+class FixedDepositDeleteView(DeleteView):
+    template_name = 'fixed-deposits/fixed_deposit_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(FixedDeposit, id=id_)
+
+    def get_success_url(self):
+        return reverse('fixed-deposits:fixed-deposit-list')
+
+def add_fixed_deposit(request):
+    # https://www.youtube.com/watch?v=Zx09vcYq1oc&list=PLLxk3TkuAYnpm24Ma1XenNeq1oxxRcYFT
+    template = 'fixed-deposits/add_fixed_deposit.html'
+    if request.method == 'POST':
+        print(request.POST)
+        if "submit" in request.POST:
+            print("submit button pressed")
+            number = request.POST['number']
+            bank_name = request.POST['bank_name']
+            start_date = request.POST['start_date']
+            user = request.POST['user']
+            time_period_days = Decimal(request.POST['time_period_days'])
+            roi = Decimal(request.POST['roi'])
+            principal = Decimal(request.POST['principal'])
+            final_val = Decimal(request.POST['final_val'])
+            goal = Decimal(request.POST['goal'])
+            notes = request.POST['notes']
+            mat_date = request.POST['mat_date']
+            add_fd_entry(number, bank_name, start_date, principal, time_period_days,
+                    final_val, user, notes, goal, roi, mat_date)
+        else:
+            print("calculate button pressed")
+            number = request.POST['number']
+            bank_name = request.POST['bank_name']
+            start_date = request.POST['start_date']
+            user = request.POST['user']
+            time_period_days = Decimal(request.POST['time_period_days'])
+            principal = Decimal(request.POST['principal'])
+            roi = Decimal(request.POST['roi'])
+            notes = request.POST['notes']
+            goal = Decimal(request.POST['goal'])
+            mat_date, val = get_maturity_value(int(principal), start_date, float(roi), int(time_period_days))
+            print("calculated value", val)
+            context = {'user':user, 'number':number, 'start_date':start_date, 'bank_name': bank_name, 'roi': roi,
+                'time_period_days': time_period_days, 'principal': principal, 'final_val':val, 'notes': notes,
+                'goal':goal, 'mat_date':mat_date}
+            return render(request, template, context=context)
+    return render(request, template)
