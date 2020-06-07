0142dbdc2a6cb2d15f04a8bc81cdc9a1db1809ac
Krishna Kuruvadi <krishnakuruvadi@gmail.com>
2020-06-07 22:03:29 +0530

    User and goal as drop down for epf

--- src/portfoliomgr/settings.py
+++ src/portfoliomgr/settings.py
@@ -77,6 +77,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'template_filters': 'portfoliomgr.template_filters',
+
+            }
         },
     },
 ]
