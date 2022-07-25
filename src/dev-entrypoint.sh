c5cb7ba5a8aa2f12196040ae10c69f3524572674
Luis Leon <luisleon309@gmail.com>
2022-07-25 16:38:04 +0000

    Added superuser creation automation. Huey and Gunicorn processes added

--- src/dev-entrypoint.sh
+++ src/dev-entrypoint.sh
@@ -3,5 +3,13 @@
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+python manage.py createsuperuser --no-input
+
+# Both processes need to run simultaneously and will remain open until the container is stopped.
+gunicorn portfoliomgr.wsgi:application --user www-data --bind 0.0.0.0:8020 --workers 3 & PIDGUNICORN=$!
+python manage.py run_huey & PIDHUEY=$!
+
+wait $PIDGUNICORN
+wait $PIDHUEY
 
 exec "$@"
