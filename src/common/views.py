3cfa2dd3626c4debdec111ef3bb24666f09752bc
lal309 <51451936+lal309@users.noreply.github.com>
2022-08-22 22:41:21 -0500

    Added metadata and versioning capabilities

--- src/common/views.py
+++ src/common/views.py
@@ -21,7 +21,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 import json
-import yaml
 from dateutil import tz
 from pytz import timezone
 from common.helper import get_preferences
