d4ace2773d930d5532311cd3fc93742ad7d6404a
Luis Leon <luisleon309@gmail.com>
2022-09-03 22:34:38 -0500

    Replace scipy with pyxirr

--- src/shared/financial.py
+++ src/shared/financial.py
@@ -1,5 +1,5 @@
 import datetime
-from scipy import optimize
+from pyxirr import xirr as py_xirr
 from dateutil.relativedelta import relativedelta
 
 def secant_method(tol, f, x0):
@@ -54,30 +54,12 @@ def xnpv(rate,cashflows):
 
     return sum([cf/(1+rate)**((t-t0).days/365.0) for (t,cf) in chron_order])
 
-def xirr(cashflows,guess=0.1):
-    """
-    Calculate the Internal Rate of Return of a series of cashflows at irregular intervals.
+def xirr(cashflows, guess):
 
-    Arguments
-    ---------
-    * cashflows: a list object in which each element is a tuple of the form (date, amount), where date is a python datetime.date object and amount is an integer or floating point number. Cash outflows (investments) are represented with negative amounts, and cash inflows (returns) are positive amounts.
-    * guess (optional, default = 0.1): a guess at the solution to be used as a starting point for the numerical solution. 
-
-    Returns
-    --------
-    * Returns the IRR as a single value
-    
-    Notes
-    ----------------
-    * The Internal Rate of Return (IRR) is the discount rate at which the Net Present Value (NPV) of a series of cash flows is equal to zero. The NPV of the series of cash flows is determined using the xnpv function in this module. The discount rate at which NPV equals zero is found using the secant method of numerical solution. 
-    * This function is equivalent to the Microsoft Excel function of the same name.
-    * For users that do not have the scipy module installed, there is an alternate version (commented out) that uses the secant_method function defined in the module rather than the scipy.optimize module's numerical solver. Both use the same method of calculation so there should be no difference in performance, but the secant_method function does not fail gracefully in cases where there is no solution, so the scipy.optimize.newton version is preferred.
-
-    """
-    
-    #return secant_method(0.0001,lambda r: xnpv(r,cashflows),guess)
+    # guess param not used in pyxirr, but it is passed to the function by the invoking code due to previous scipy library requirements.
     try:
-        val = optimize.newton(lambda r: xnpv(r,cashflows),guess)
+        val = py_xirr(cashflows)
+        print(f' got {val} for {cashflows}')
         if val > 10000000:
             print(f'unreasonable returns {val}.  returning 0 instead')
             return 0
