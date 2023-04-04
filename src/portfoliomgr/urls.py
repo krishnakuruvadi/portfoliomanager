ccc231adf225ab9b0a3ac814052c4a9379cdf2f5
Luis Leon <luisleon309@gmail.com>
2023-04-04 01:42:46 -0500

    Adding authentication to entire app

--- src/portfoliomgr/urls.py
+++ src/portfoliomgr/urls.py
@@ -1,52 +1,40 @@
-"""portfoliomgr URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from pages.views import home_view, GetInvestmentData, Export
 #from pages.views import ChartData
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('ppf/', include('ppf.urls')),
-    path('ssy/', include('ssy.urls')),
-    path('epf/', include('epf.urls')),
-    path('espp/', include('espp.urls')),
-    path('goal/', include('goal.urls')),
-    path('user/', include('users.urls')),
-    path('fixed-deposit/', include('fixed_deposit.urls')),
-    path('mutualfunds/', include('mutualfunds.urls')),
-    path('rsu/', include('rsu.urls')),
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('shares/', include('shares.urls')),
-    path('reports/', include('reports.urls')),
-    path('common/', include('common.urls')),
-    path('calculator/', include('calculator.urls')),
+    path('alerts/', include('alerts.urls')),
     path('api/chart/investmentdata/', GetInvestmentData.as_view()),
     path('api/export/', Export.as_view()),
-    path('alerts/', include('alerts.urls')),
-    path('markets/', include('markets.urls')),
-    path('tasks/', include('tasks.urls')),
-    path('retirement_401k/', include('retirement_401k.urls')),
-    path('tax/', include('tax.urls')),
-    path('insurance/', include('insurance.urls')),
-    path('gold/', include('gold.urls')),
     path('bankaccounts/', include('bankaccounts.urls')),
-    path('crypto/', include('crypto.urls'))
+    path('calculator/', include('calculator.urls')),
+    path('common/', include('common.urls')),
+    path('crypto/', include('crypto.urls')),
+    path('epf/', include('epf.urls')),
+    path('espp/', include('espp.urls')),
+    path('fixed-deposit/', include('fixed_deposit.urls')),
+    path('goal/', include('goal.urls')),
+    path('gold/', include('gold.urls')),
+    path('insurance/', include('insurance.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='account-login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='account-logout'),
+    path('markets/', include('markets.urls')),
+    path('mutualfunds/', include('mutualfunds.urls')),
+    path('ppf/', include('ppf.urls')),
+    path('reports/', include('reports.urls')),
+    path('retirement_401k/', include('retirement_401k.urls')),
+    path('rsu/', include('rsu.urls')),
+    path('shares/', include('shares.urls')),
+    path('ssy/', include('ssy.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('tax/', include('tax.urls')),
+    path('user/', include('users.urls')), 
 ]
 
 if settings.DEBUG:
