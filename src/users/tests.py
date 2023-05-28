395004812a8a362b243dbd7c1cdad9974da6a970
Krishna Kuruvadi <krishnakuruvadi@gmail.com>
2023-05-28 01:51:12 -0700

    add user login to pipeline test

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from .models import User
 import datetime
+from django.db import IntegrityError
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
 # Create your tests here.
 
 def add_default_user():
@@ -9,3 +14,19 @@ def add_default_user():
         email='ramakrishna@test.com',
         dob=datetime.date(year=1990,month=12,day=10)
     )
+
+def add_non_admin_account():
+    from django.contrib.auth.models import User
+    try:
+        User.objects.create_user('johndoe', 'johndoe@gmail.com', 'johnpassword')
+    except IntegrityError:
+        print(f'non admin user account already exists')
+    except Exception as ex:
+        print(f'exception adding non admin account {ex}')
+
+
+def login_non_admin_account(browser):
+    browser.find_element(By.NAME, 'username').send_keys('johndoe')
+    browser.find_element(By.NAME, 'password').send_keys('johnpassword')
+    browser.find_element(By.XPATH, '//button[normalize-space()="Login"]').click()
+    time.sleep(5)
